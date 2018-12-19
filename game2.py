f2b594b55daf87d270d12d80e9b6cea7b86724d4
Army-Maulana <armymaulanaps28@gmail.com>
2018-12-19 09:40:18 +0700

    update

--- game2.py
+++ game2.py
@@ -38,7 +38,7 @@ def checker(buttons):
 		exit()
 		
 buttons=StringVar()
-button1 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button1))
+button1 = Button(tk,text=" ",font=('Times 40 bold'), heigh = 4, width = 8, command=lambda:checker(button1))
 button1.grid(row=1, column=0,sticky = S+N+E+W)
 button2 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button2))
 button2.grid(row=1, column=1,sticky = S+N+E+W)
